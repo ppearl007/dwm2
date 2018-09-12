052ecd734a2803777aff506cbadb6581d5629a64
Your Name <you@example.com>
2018-09-12 15:26:07 -0500

    Some landing page js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+    setTimeout(function(){
+        $('.banner-text').removeClass('animate');
+    }, 00);
+
+    $(window).scroll(function(){
+        $('#vision, #mission').fadeIn(2000);
+    });
+
+}); 
+
+
+// document.addEventListener('load', function(){
+//     var el = document.getElementsByClassName("banner-text");
+//     for (i=0; i<el.length; i++)
+//         el[i].style.height = 100 +'px';
+//     }
+// )
+
